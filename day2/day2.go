cb5fffdd72a66c9765322cce78209dca7c9083bc
Ionis-Kutrolli <ikutrolli2000@gmail.com>
2024-12-10 23:48:45 -0500

    Day 2 completed with a terrible but fun solution

--- day2/day2.go
+++ day2/day2.go
@@ -1,8 +1,13 @@
 package day2
 
 import (
-	"adventofcode/dayhandler"
 	"fmt"
+	"math"
+	"slices"
+	"strconv"
+	"strings"
+
+	"adventofcode/dayhandler"
 )
 
 func init() {
@@ -12,15 +17,90 @@ func init() {
 type dayHandler struct {
 }
 
-// AdvancedHandler implements dayhandler.dayHandler.
-func (d dayHandler) AdvancedHandler(string) error {
-	return fmt.Errorf("unimplemented")}
-
-// BasicHandler implements dayhandler.dayHandler.
-func (d dayHandler) BasicHandler(string) error {
-	return fmt.Errorf("unimplemented")
-}
-
 func newDayHandler() dayHandler {
 	return dayHandler{}
+}
+
+// AdvancedHandler implements dayhandler.dayHandler.
+func (d dayHandler) AdvancedHandler(s string) error {
+	lines, err := inputHandler(s)
+	if err != nil {
+		return err
+	}
+	numSafe := 0
+	for _, line := range lines {
+		for i := -1; i < len(line); i++ {
+			clone := line
+			if i != -1 {
+				clone = slices.Clone(line)
+				clone = slices.Delete(clone, i, i+1)
+			}
+			increasing := 0.0
+			safe := slices.IsSortedFunc(clone, func(a, b int) int {
+				diff := float64(a - b)
+				absDiff := math.Abs(diff)
+				if absDiff < 1 || absDiff > 3 {
+					return -1
+				}
+				if increasing == 0 {
+					increasing = math.Copysign(1, diff)
+					return 1
+				} else {
+					return int(math.Copysign(1, diff*increasing))
+				}
+			})
+			if safe {
+				numSafe++
+				break
+			}
+		}
+	}
+	fmt.Printf("Number Safe: %d", numSafe)
+	return nil
+}
+
+// BasicHandler implements dayhandler.dayHandler.
+func (d dayHandler) BasicHandler(s string) error {
+	lines, err := inputHandler(s)
+	if err != nil {
+		return err
+	}
+	numSafe := 0
+	for _, line := range lines {
+		increasing := 0.0
+		safe := slices.IsSortedFunc(line, func(a, b int) int {
+			diff := float64(a - b)
+			absDiff := math.Abs(diff)
+			if absDiff < 1 || absDiff > 3 {
+				return -1
+			}
+			if increasing == 0 {
+				increasing = math.Copysign(1, diff)
+				return 1
+			} else {
+				return int(math.Copysign(1, diff*increasing))
+			}
+		})
+		if safe {
+			numSafe++
+		}
+	}
+	fmt.Printf("Number Safe: %d", numSafe)
+	return nil
+}
+
+func inputHandler(s string) ([][]int, error) {
+	lines := strings.Split(s, "\n")
+	parsedLines := [][]int{}
+	for i, line := range lines {
+		parsedLines = append(parsedLines, []int{})
+		for _, num := range strings.Split(line, " ") {
+			val, err := strconv.ParseInt(strings.TrimSpace(num), 10, 0)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse int %s: %w", num, err)
+			}
+			parsedLines[i] = append(parsedLines[i], int(val))
+		}
+	}
+	return parsedLines, nil
 }
